3629f510ce3b66eba6d3458f4c20d214aa927dd2
Midget807 <phu7mai@gmail.com>
2024-07-08 01:03:21 +0930

    Started Triple shot enchant

---
+++ src/main/java/net/midget807/afmweapons/mixin/CrossbowItemMixin.java
@@ -0,0 +1,104 @@
+package net.midget807.afmweapons.mixin;
+
+import net.midget807.afmweapons.enchantment.ModEnchantments;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.inventory.StackReference;
+import net.minecraft.item.ArrowItem;
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ClickType;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CrossbowItem.class)
+public abstract class CrossbowItemMixin {
+
+    @Shadow
+    private static PersistentProjectileEntity createArrow(World world, LivingEntity entity, ItemStack crossbow, ItemStack arrow) {
+        ArrowItem arrowItem = (ArrowItem)(arrow.getItem() instanceof ArrowItem ? arrow.getItem() : Items.ARROW);
+        PersistentProjectileEntity persistentProjectileEntity = arrowItem.createArrow(world, arrow, entity);
+        if (entity instanceof PlayerEntity) {
+            persistentProjectileEntity.setCritical(true);
+        }
+
+        persistentProjectileEntity.setSound(SoundEvents.ITEM_CROSSBOW_HIT);
+        persistentProjectileEntity.setShotFromCrossbow(true);
+        int i = EnchantmentHelper.getLevel(Enchantments.PIERCING, crossbow);
+        if (i > 0) {
+            persistentProjectileEntity.setPierceLevel((byte)i);
+        }
+
+        return persistentProjectileEntity;
+    }
+
+    @Inject(method = "shoot", at = @At("HEAD"),cancellable = true)
+    private static void afmw$tripleShot(World world, LivingEntity shooter, Hand hand, ItemStack crossbow, ItemStack projectile, float soundPitch, boolean creative, float speed, float divergence, float simulated, CallbackInfo ci) {
+        if (!world.isClient) {
+            int tripleShotLevel = EnchantmentHelper.getLevel(ModEnchantments.TRIPLE_SHOT, crossbow);
+            if (tripleShotLevel > 0) {
+                ProjectileEntity projectileEntity;
+                projectileEntity = createArrow(world, shooter, crossbow, projectile);
+                world.spawnEntity(projectileEntity);
+            }
+        }
+    }
+    //Methods from BundleItem
+    /*
+    @Unique
+    public boolean onStackClicked(ItemStack stack, Slot slot, ClickType clickType, PlayerEntity player) {
+        if (clickType != ClickType.RIGHT) {
+            return false;
+        } else {
+            ItemStack itemStack = slot.getStack();
+            if (itemStack.isEmpty()) {
+                this.playRemoveOneSound(player);
+                removeFirstStack(stack).ifPresent(removedStack -> addToBundle(stack, slot.insertStack(removedStack)));
+            } else if (itemStack.getItem().canBeNested()) {
+                int i = (64 - getBundleOccupancy(stack)) / getItemOccupancy(itemStack);
+                int j = addToBundle(stack, slot.takeStackRange(itemStack.getCount(), i, player));
+                if (j > 0) {
+                    this.playInsertSound(player);
+                }
+            }
+
+            return true;
+        }
+    }
+    public boolean onClicked(ItemStack stack, ItemStack otherStack, Slot slot, ClickType clickType, PlayerEntity player, StackReference cursorStackReference) {
+        if (clickType == ClickType.RIGHT && slot.canTakePartial(player)) {
+            if (otherStack.isEmpty()) {
+                removeFirstStack(stack).ifPresent(itemStack -> {
+                    this.playRemoveOneSound(player);
+                    cursorStackReference.set(itemStack);
+                });
+            } else {
+                int i = addToBundle(stack, otherStack);
+                if (i > 0) {
+                    this.playInsertSound(player);
+                    otherStack.decrement(i);
+                }
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+     */
+
+}
