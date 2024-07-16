572d82b7107fe38c6ba185570ce222dbde07c49c
Midget807 <phu7mai@gmail.com>
2024-07-17 01:07:38 +0930

    Triple shot three shots

--- src/main/java/net/midget807/afmweapons/mixin/CrossbowItemMixin.java
+++ src/main/java/net/midget807/afmweapons/mixin/CrossbowItemMixin.java
@@ -17,6 +17,7 @@ import net.minecraft.screen.slot.Slot;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.ClickType;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -28,6 +29,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
@@ -63,39 +65,40 @@ public abstract class CrossbowItemMixin extends Item {
         nbtCompound.putBoolean("Charged", charged);
     }
 
-    /*
-        @Inject(method = "shoot", at = @At("HEAD"),cancellable = true)
-        private static void afmw$tripleShot(World world, LivingEntity shooter, Hand hand, ItemStack crossbow, ItemStack projectile, float soundPitch, boolean creative, float speed, float divergence, float simulated, CallbackInfo ci) {
-            if (!world.isClient) {
-                int tripleShotLevel = EnchantmentHelper.getLevel(ModEnchantments.TRIPLE_SHOT, crossbow);
-                if (tripleShotLevel > 0) {
-                    ProjectileEntity projectileEntity;
-                    projectileEntity = createArrow(world, shooter, crossbow, projectile);
-                    world.spawnEntity(projectileEntity);
-                }
-            }
-        }
-        */
+
+    @Shadow
+    private static boolean loadProjectile(LivingEntity shooter, ItemStack crossbow, ItemStack projectile, boolean simulated, boolean creative) {
+        return false;
+    }
     @Unique
-    private static boolean afmw$hasTripleShot(LivingEntity shooter, ItemStack crossbow) {
-        if (shooter.getStackInHand(shooter.preferredHand).isOf(Items.CROSSBOW)) {
-            int i = EnchantmentHelper.getLevel(ModEnchantments.TRIPLE_SHOT, crossbow);
-            return i != 0;
-        } else {
-            return false;
-        }
+    private static boolean afmw$hasTripleShot(ItemStack crossbow) {
+        return EnchantmentHelper.getLevel(ModEnchantments.TRIPLE_SHOT, crossbow) != 0;
     }
     @Unique
     private static int afmw$numberOfShots;
+    @Inject(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/CrossbowItem;setCharged(Lnet/minecraft/item/ItemStack;Z)V"))
+    private void afmw$loadNumberOfShots(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfo ci) {
+        if(afmw$hasTripleShot(stack)) {
+            afmw$numberOfShots = 3;
+        }
+    }
 
-    @Inject(method = "use", at = @At(value = "HEAD"), cancellable = true)
+    @Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/CrossbowItem;setCharged(Lnet/minecraft/item/ItemStack;Z)V", shift = At.Shift.AFTER), cancellable = true)
     private void afmw$tripleShot(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
-        ItemStack stack = user.getStackInHand(hand);
-        user.sendMessage(Text.literal("Level Test 1: " + EnchantmentHelper.getLevel(ModEnchantments.TRIPLE_SHOT, stack)));
-        if (afmw$hasTripleShot(user, stack)) {
-            user.sendMessage(Text.literal("Level Test 2: " + EnchantmentHelper.getLevel(ModEnchantments.TRIPLE_SHOT, stack)));
-            setCharged(stack, true);
-            cir.setReturnValue(TypedActionResult.consume(stack));
+        ItemStack crossbow = user.getStackInHand(hand);
+        ItemStack projectile = user.getProjectileType(crossbow);
+
+        if (afmw$hasTripleShot(crossbow) && afmw$numberOfShots >= 0) {
+            afmw$numberOfShots--;
+            if (!user.isCreative()) {
+                projectile.split(1);
+                if (projectile.isEmpty() && user instanceof PlayerEntity) {
+                    user.getInventory().removeOne(projectile);
+                }
+            }
+            loadProjectile(user, crossbow, user.getProjectileType(crossbow), true, false);
+            setCharged(crossbow,true);
+            cir.setReturnValue(TypedActionResult.consume(crossbow));
         }
     }
 
