431def329c57a00fed4c4be1f821aa3f685ff1b9
Midget807 <phu7mai@gmail.com>
2024-07-22 18:46:18 +0930

    Triple Shot

--- src/main/java/net/midget807/afmweapons/mixin/CrossbowItemMixin.java
+++ src/main/java/net/midget807/afmweapons/mixin/CrossbowItemMixin.java
@@ -5,24 +5,20 @@ import net.midget807.afmweapons.item.afmw.client.TripleShotTooltipData;
 import net.minecraft.client.item.TooltipData;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
-import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.inventory.StackReference;
 import net.minecraft.item.*;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
-import net.minecraft.potion.PotionUtil;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.ClickType;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.collection.DefaultedList;
@@ -33,15 +29,12 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
-import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
 
 @Mixin(CrossbowItem.class)
@@ -78,7 +71,23 @@ public abstract class CrossbowItemMixin extends Item {
 
     @Shadow
     private static boolean loadProjectile(LivingEntity shooter, ItemStack crossbow, ItemStack projectile, boolean simulated, boolean creative) {
-        return false;
+        if (projectile.isEmpty()) {
+            return false;
+        } else {
+            boolean bl = creative && projectile.getItem() instanceof ArrowItem;
+            ItemStack itemStack;
+            if (!bl && !creative && !simulated) {
+                itemStack = projectile.split(1);
+                if (projectile.isEmpty() && shooter instanceof PlayerEntity) {
+                    ((PlayerEntity)shooter).getInventory().removeOne(projectile);
+                }
+            } else {
+                itemStack = projectile.copy();
+            }
+
+            putProjectile(crossbow, itemStack);
+            return true;
+        }
     }
 
     @Shadow
@@ -86,6 +95,17 @@ public abstract class CrossbowItemMixin extends Item {
     }
 
     @Shadow public abstract Predicate<ItemStack> getProjectiles();
+
+
+    @Shadow private boolean charged;
+
+    @Shadow private boolean loaded;
+
+    @Shadow
+    public static boolean isCharged(ItemStack stack) {
+        NbtCompound nbtCompound = stack.getNbt();
+        return nbtCompound != null && nbtCompound.getBoolean("Charged");
+    }
 
     @Unique
     private static boolean afmw$hasTripleShot(ItemStack crossbow) {
@@ -104,14 +124,30 @@ public abstract class CrossbowItemMixin extends Item {
             //=====
         }
     }
+
     @Redirect(method = "loadProjectiles", at = @At(value = "INVOKE", target ="Lnet/minecraft/entity/LivingEntity;getProjectileType(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;"))
-    private static ItemStack afmw$loadInsertedProjectile(LivingEntity shooter, ItemStack projectile) {
+    private static ItemStack afmw$loadInsertedProjectile(LivingEntity shooter, ItemStack crossbow) {
         DefaultedList<ItemStack> defaultedList = DefaultedList.of();
-        getInsertedStacks(shooter.getActiveItem()).forEach(defaultedList::add);
+        getInsertedStacks(crossbow).forEach(defaultedList::add);
         if (!defaultedList.isEmpty()) {
             return defaultedList.get(0);
         } else {
-            return shooter.getProjectileType(shooter.getActiveItem());
+            return shooter.getProjectileType(crossbow);
+        }
+    }
+
+    @Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/TypedActionResult;fail(Ljava/lang/Object;)Lnet/minecraft/util/TypedActionResult;"), cancellable = true)
+    private void afmw$loadFirstShot(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+        ItemStack crossbow = user.getStackInHand(hand);
+        DefaultedList<ItemStack> defaultedList = DefaultedList.of();
+        getInsertedStacks(crossbow).forEach(defaultedList::add);
+        if (!defaultedList.isEmpty()) {
+            if (!isCharged(crossbow)) {
+                this.charged = false;
+                this.loaded = false;
+                user.setCurrentHand(hand);
+            }
+            cir.setReturnValue(TypedActionResult.consume(crossbow));
         }
     }
 
@@ -137,15 +173,19 @@ public abstract class CrossbowItemMixin extends Item {
             if (!world.isClient) {
                 afmw$numberOfShots--;
             }
-            //Debug
+            //Debug TODO debug
             if (!world.isClient) {
                 user.sendMessage(Text.literal("Number of shots: " + afmw$numberOfShots));
             }
             //=====
             if (!user.isCreative()) {
-                projectile.split(1);
-                if (projectile.isEmpty() && user instanceof PlayerEntity) {
-                    user.getInventory().removeOne(projectile);
+                if (!defaultedList.isEmpty()) {
+                    removeAmountFromTripleShot(crossbow, 0);
+                } else {
+                    projectile.split(1);
+                    if (projectile.isEmpty() && user instanceof PlayerEntity) {
+                        user.getInventory().removeOne(projectile);
+                    }
                 }
             }
             loadProjectile(user, crossbow, projectile, true, false);
@@ -154,7 +194,24 @@ public abstract class CrossbowItemMixin extends Item {
         }
     }
 
-    //Triple Shot Storage
+    //Triple Shot Storage ===
+
+
+    @Unique
+    private static void removeAmountFromTripleShot(ItemStack crossbow, int index) {
+        NbtCompound crossbowNbt = crossbow.getOrCreateNbt();
+        if (crossbowNbt.contains(ITEMS_KEY)) {
+            NbtList nbtList = crossbowNbt.getList(ITEMS_KEY, NbtElement.COMPOUND_TYPE);
+            if (!nbtList.isEmpty()) {
+                NbtCompound stackNbt = nbtList.getCompound(index);
+                ItemStack stack = ItemStack.fromNbt(stackNbt);
+                stack.decrement(1);
+                stack.writeNbt(stackNbt);
+                nbtList.remove(stackNbt);
+                nbtList.add(index, stackNbt);
+            }
+        }
+    }
 
     @Unique
     private static final String ITEMS_KEY = "TripleShotItems";
@@ -197,21 +254,24 @@ public abstract class CrossbowItemMixin extends Item {
     private static float getAmountFilled(ItemStack stack) {
         return (float) getCrossbowOccupancy(stack) / MAX_STORAGE;
     }
+
+    //Crossbow Stack clicks Arrow Stack
     @Override
-    public boolean onStackClicked(ItemStack stack, Slot slot, ClickType clickType, PlayerEntity player) {
+    public boolean onStackClicked(ItemStack crossbowStack, Slot slot, ClickType clickType, PlayerEntity player) {
         DefaultedList<ItemStack> defaultedList = DefaultedList.of();
-        getInsertedStacks(stack).forEach(defaultedList::add);
+        getInsertedStacks(crossbowStack).forEach(defaultedList::add);
         if (clickType != ClickType.RIGHT) {
             return false;
         } else {
-            ItemStack itemStack = slot.getStack();
-            if (itemStack.isEmpty()) {
+            ItemStack slotStack = slot.getStack();
+            if (slotStack.isEmpty()) {
                 this.playRemoveOneSound(player);
-                removeFirstStack(stack).ifPresent(removedStack -> addToCrossbow(stack, slot.insertStack(removedStack)));
-            } else if (itemStack.getItem().canBeNested() && itemStack.isIn(ItemTags.ARROWS) && afmw$hasTripleShot(stack) && defaultedList.size() < 3) {
-                int i = getI(itemStack, defaultedList);
-                int j = addToCrossbow(stack, slot.takeStackRange(itemStack.getCount(), i, player));
+                removeFirstStack(crossbowStack).ifPresent(removedStack -> addToCrossbow(crossbowStack, slot.insertStack(removedStack)));
+            } else if (slotStack.getItem().canBeNested() && slotStack.isIn(ItemTags.ARROWS) && afmw$hasTripleShot(crossbowStack) && defaultedList.size() < 3) {
+
+                int j = addToCrossbow(crossbowStack, slotStack);
                 if (j > 0) {
+                    slotStack.decrement(j);
                     this.playInsertSound(player);
                 }
             }
@@ -238,21 +298,22 @@ public abstract class CrossbowItemMixin extends Item {
         return i;
     }
 
+    //Arrow Stack Clicks Crossbow Stack
     @Override
-    public boolean onClicked(ItemStack stack, ItemStack otherStack, Slot slot, ClickType clickType, PlayerEntity player, StackReference cursorStackReference) {
+    public boolean onClicked(ItemStack crossbowStack, ItemStack arrowStack, Slot slot, ClickType clickType, PlayerEntity player, StackReference cursorStackReference) {
         DefaultedList<ItemStack> defaultedList = DefaultedList.of();
-        getInsertedStacks(stack).forEach(defaultedList::add);
+        getInsertedStacks(crossbowStack).forEach(defaultedList::add);
         if (clickType == ClickType.RIGHT && slot.canTakePartial(player)) {
-            if (otherStack.isEmpty()) {
-                removeFirstStack(stack).ifPresent(itemStack -> {
+            if (arrowStack.isEmpty()) {
+                removeFirstStack(crossbowStack).ifPresent(itemStack -> {
                     this.playRemoveOneSound(player);
                     cursorStackReference.set(itemStack);
                 });
-            } else if (otherStack.isIn(ItemTags.ARROWS) && afmw$hasTripleShot(stack) && defaultedList.size() < 3){
-                int i = addToCrossbow(stack, otherStack);
+            } else if (arrowStack.isIn(ItemTags.ARROWS) && afmw$hasTripleShot(crossbowStack) && defaultedList.size() < 3){
+                int i = addToCrossbow(crossbowStack, arrowStack);
                 if (i > 0) {
                     this.playInsertSound(player);
-                    otherStack.decrement(i);
+                    arrowStack.decrement(i);
                 }
             }
             return true;
@@ -280,32 +341,34 @@ public abstract class CrossbowItemMixin extends Item {
     private static int addToCrossbow(ItemStack crossbow, ItemStack stack) {
         DefaultedList<ItemStack> defaultedList = DefaultedList.of();
         getInsertedStacks(crossbow).forEach(defaultedList::add);
-        if (!stack.isEmpty() && stack.getItem().canBeNested() && stack.isIn(ItemTags.ARROWS) && afmw$hasTripleShot(crossbow) && defaultedList.size() < 3) {
+        if (!stack.isEmpty() && stack.getItem().canBeNested() && stack.isIn(ItemTags.ARROWS) && afmw$hasTripleShot(crossbow) && defaultedList.size() <= 3) {
             NbtCompound nbtCompound = crossbow.getOrCreateNbt();
             if (!nbtCompound.contains(ITEMS_KEY)) {
                 nbtCompound.put(ITEMS_KEY, new NbtList());
             }
-            int i = getCrossbowOccupancy(crossbow);
-            int j = getItemOccupancy(stack);
-            int k = getK(stack, defaultedList);
-            if (k == 0) {
-                return 0;
-            } else {
-                NbtList nbtList = nbtCompound.getList(ITEMS_KEY, NbtElement.COMPOUND_TYPE);
-                Optional<NbtCompound> optional = canMergeStack(stack, nbtList);
-                if (optional.isPresent()) {
-                    NbtCompound nbtCompound2 = (NbtCompound) optional.get();
-                    ItemStack itemStack = ItemStack.fromNbt(nbtCompound2);
-                    itemStack.increment(k);
-                    itemStack.writeNbt(nbtCompound2);
-                    nbtList.remove(nbtCompound2);
-                    nbtList.add(0, nbtCompound2);
-                } else {
-                    ItemStack itemStack2 = stack.copyWithCount(k);
-                    NbtCompound nbtCompound3 = new NbtCompound();
-                    itemStack2.writeNbt(nbtCompound3);
-                    nbtList.add(0, nbtCompound3);
+            NbtList nbtList = nbtCompound.getList(ITEMS_KEY, NbtElement.COMPOUND_TYPE);
+            Optional<NbtCompound> optional = canMergeStack(stack, nbtList);
+            if (optional.isPresent()) {
+                NbtCompound nbtCompound2 = (NbtCompound) optional.get();
+                ItemStack stackInCrossbow = ItemStack.fromNbt(nbtCompound2);
+                int k = Math.min(64 - stackInCrossbow.getCount(), stack.getCount());
+                if (k == 0) {
+                    return 0;
                 }
+                stackInCrossbow.increment(k);
+                stackInCrossbow.writeNbt(nbtCompound2);
+                nbtList.remove(nbtCompound2);
+                nbtList.add(0, nbtCompound2);
+                return  k;
+            } else {
+                int k  = stack.getCount();
+                if (k == 0) {
+                    return 0;
+                }
+                ItemStack itemStack2 = stack.copyWithCount(k);
+                NbtCompound nbtCompound3 = new NbtCompound();
+                itemStack2.writeNbt(nbtCompound3);
+                nbtList.add(0, nbtCompound3);
                 return k;
             }
         } else {
@@ -324,7 +387,7 @@ public abstract class CrossbowItemMixin extends Item {
             }  else if (defaultedList.size() > 2 && stack.isOf(defaultedList.get(2).getItem()) && stack.getName() == defaultedList.get(2).getName()) {
                 k = Math.min((64 - defaultedList.get(2).getCount()), stack.getCount());
             } else {
-                k = stack.getCount();
+                k = 0;
             }
         } else {
             k = stack.getCount();
